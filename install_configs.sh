b65e46c5a0e1c6e2df29bbca74304013c9f002fc
Turtle-In-Space <elias.svensson63@gmail.com>
2025-06-05 20:27:45 +0200

    make instal script real nice

--- install_configs.sh
+++ install_configs.sh
@@ -1,23 +1,49 @@
 #!/bin/bash
 
-# Define source directory (assumes script is run from the directory containing these files)
 SRC_DIR="$(pwd)"
 
+printInfo() {
+    local msg=$1
+
+    echo "[*]" $msg
+}
+
+# Function to prompt and copy config with backup
+copy_config() {
+    local name=$1
+    local src=$2
+    local dest=$3
+
+    read -p "[?] Do you want to copy the $name config? (y/n): " answer
+    case $answer in
+        [Yy]* )
+            if [ -e "$dest" ]; then
+                printInfo "Backing up existing $name config..."
+                cp -r "$dest" "${dest}.bak"
+            fi
+            printInfo "Copying $name config..."
+            mkdir -p "$(dirname "$dest")"
+            cp -r "$src" "$dest"
+            ;;
+        * )
+            printInfo "Skipped $name config."
+            ;;
+    esac
+}
+
 # Copy bat config
-echo "[*] Copying bat config..."
-cp -r "$SRC_DIR/bat/"* ~/.config/bat/
+copy_config "bat" "$SRC_DIR/bat" "$HOME/.config/bat"
 
 # Copy kitty config
-echo "[*] Copying kitty config..."
-cp -r "$SRC_DIR/kitty/"* ~/.config/kitty/
+copy_config "kitty" "$SRC_DIR/kitty" "$HOME/.config/kitty"
 
 # Copy .zshrc
-echo "[*] Copying .zshrc..."
-cp "$SRC_DIR/.zshrc" ~/.zshrc
+copy_config ".zshrc" "$SRC_DIR/.zshrc" "$HOME/.zshrc"
 
-# Install needed plugins
-echo "[*] Installing needed plugins..."
+# download needed plugins
+printInfo "Downloading git repos..."
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 
-echo "[*] Done."
+printInfo "Installation script completed."
